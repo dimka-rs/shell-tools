79987039676294e4eca8c24492fc646e3451024a
Dmitry Koroban <dmitry@radiofid.ru>
2019-05-30 17:17:10 +0300

    update up.sh

--- up.sh
+++ up.sh
@@ -1,12 +1,38 @@
 #!/bin/bash
-START=`grep Awake /var/log/pm-suspend.log|tail -n 1|sed -e 's/:\ .*$//g'`
-START_S=`date -d "$START" +%s`
+ICON_FILE=/usr/share/icons/hicolor/scalable/apps/mate-panel-clock.svg
+
+# get start time suspend
+#START=`grep Awake /var/log/pm-suspend.log|tail -n 1|sed -e 's/:\ .*$//g' 2>/dev/null`
+# if none, try older log
+#[ -z "$START" ] && START=`grep Awake /var/log/pm-suspend.log.1|tail -n 1|sed -e 's/:\ .*$//g'`
+# if none, try hibernate
+[ -z "$START" ] && START=`grep 'PM: hibernation exit' /var/log/kern.log| tail -n 1| awk '{print $3}'`
+
+START_S=`date +%s -d $START 2>/dev/null`
 NOW=`date +%s`
 DIFF=$(( NOW - START_S ))
-DIFF_MAX=$(( 8,5 * 60 * 60 ))
-PRETTY=`date -d@$DIFF -u +%H:%M:%S`
+DIFF_WORK=30600	## 8.5*60*60
+DIFF_MAX=86400	## 24*60*60
 
-if [ ${DIFF} -gt ${DIFF_MAX} ] || [ -n "$1" ]; then
-	notify-send "Time at work" "$PRETTY" -i /usr/share/icons/hicolor/scalable/apps/mate-panel-clock.svg
+if [ "$DIFF" -gt "$DIFF_MAX" ]; then
+	## if time is more than limit, try hibernaion log
+	HIBER=`grep 'PM: hibernation exit' /var/log/kern.log 2>/dev/null`
+	[ -z "$HIBER" ] && HIBER=`grep 'PM: hibernation exit' /var/log/kern.log.1`
+
+	[ -n "$1" ] && notify-send "ERR" "Max time exceeded! \n$HIBER" -i $ICON_FILE
+	exit 0
+fi
+
+LEFT=$(( DIFF_WORK - DIFF ))
+if [ $LEFT -gt 0 ]; then
+	LEFT_P=`date -d@$LEFT -u +%H:%M:%S`
+	LEFT_S="$LEFT_P left"
+else
+	LEFT_S="Time to go home"
+fi
+
+if [ ${DIFF} -gt ${DIFF_WORK} ] || [ -n "$1" ]; then
+	PRETTY=`date -d@$DIFF -u +%H:%M:%S`
+	notify-send "${PRETTY} at work" "${LEFT_S}" -i $ICON_FILE
 fi
 
